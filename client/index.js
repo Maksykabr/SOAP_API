f67814fbc68e4a365455c71ee4e194c7e40fb4e6
Maksykabr <95287886+Maksykabr@users.noreply.github.com>
2023-10-31 21:24:20 +0200

    thrid_commit

--- client/index.js
+++ client/index.js
@@ -1,23 +1,10 @@
 const soap = require('soap');
 
 const url = 'http://localhost:8000/wsdl?wsdl';
+const args = { name: 'Maks' };
 
 soap.createClient(url, function(err, client) {
-  if (err) {
-    console.error(err);
-  } else {
-    const nameToSend = 'World';
-    client.MyFunction({ name: nameToSend }, function(err, result) {
-      if (err) {
-        console.error(err);
-      } else {
-        const serviceName = result.definitions.attributes.name;
-        const targetNamespace = result.definitions.attributes.targetNamespace;
-
-        console.log("Service Name:", serviceName);
-        console.log("Target Namespace:", targetNamespace);
-        console.log("Data Sent:", nameToSend);
-      }
-    });
-  }
+  client.MyService.MyPort.MyFunction(args, function(err, result) {
+    console.log(result);
+  });
 });
--- server/app.js
+++ server/app.js
@@ -1,29 +1,26 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const soap = require('soap');
 
+// Define the service
 const service = {
   MyService: {
     MyPort: {
       MyFunction: function(args) {
         return {
-            name: args.name
+          name: args.name
         };
-      },
+      }
     }
   }
 };
 
-const app = express();
-const xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
-
-
-app.use('/wsdl', (req, res) => {
-  res.set('Content-Type', 'text/xml');
-  res.status(200).send(xml);
+// Create a SOAP server
+const xml = fs.readFileSync('myservice.wsdl', 'utf8');
+const server = http.createServer(function(request, response) {
+  response.end('404: Not Found: ' + request.url);
 });
 
-soap.listen(app, '/wsdl', service, xml);
-
-app.listen(8000, function() {
-  console.log('SOAP-сервер запущено на порту 8000');
-});
+server.listen(8000);
+soap.listen(server, '/wsdl', service, xml);
+console.log('SOAP server listening at http://localhost:8000/wsdl');
